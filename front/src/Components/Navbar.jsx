99f6cfaed1c64eadb147a4fa1c3594d637a4bf37
GeoNnim <yy4338@gamil.com>
2024-08-23 11:29:35 +0900

    1 front

---
+++ front/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container">
+        <div className="wrapper">
+          <div className="logo"></div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
